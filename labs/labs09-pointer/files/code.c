d26aa346038d66e6d7697eecf850dd9eda32c8c7
Tae <65070089@kmitl.ac.th>
2024-09-03 15:31:22 +0700

    Updated Lab 09-03

--- labs/labs09-pointer/files/code.c
+++ labs/labs09-pointer/files/code.c
@@ -1,58 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Comparison function for qsort (ascending order)
-int compare(const void *a, const void *b);
-
 int main() {
-    int *arr = NULL;        // Pointer to the dynamic array
-    int size = 0;           // Current number of elements in the array
-    int capacity = 10;      // Initial capacity of the array
-    int num;                // Variable to store user input
+    int n, m, i;
+    char *str, *ptr;
 
-    // Allocate initial memory for the array
-    arr = (int*) malloc(capacity * sizeof(int));
+    scanf("%d", &n);
+    scanf("%d", &m);
 
-    // Input loop: Read integers until the user enters -1
-    while (1) {
-        scanf(" %d", &num);     // Read an integer from the user
-        if (num == -1)          // Stop if the user enters -1
-            break;
+    str = (char *)malloc((n + 1) * sizeof(char));
 
-        // Resize the array if it's full
-        if (size >= capacity) {
-            capacity *= 2;                                          // Double the capacity
-            int *temp = (int*)realloc(arr, capacity * sizeof(int)); // Reallocate memory for the array with the new capacity
-            arr = temp;                                             // Update 'arr' to point to the newly allocated memory
-        }
+    scanf(" %[^\n]s", str);
 
-        // Add the new element to the array using pointer arithmetic
-        *(arr + size++) = num;
+    ptr = str;
+    while(ptr < (str + n)) {
+        printf("%c", *ptr);
+        ptr += m;
     }
 
-    // Print the original array
-    printf("You entered: ");
-    int *ptr = arr;                 // Initialize a pointer to traverse the array
-    while (ptr < (arr + size))      // Loop until the end of the array
-        printf("%d ", *ptr++);      // Print the current element and move the pointer
     printf("\n");
-
-    // Sort the array using qsort
-    qsort(arr, size, sizeof(int), compare);
-
-    // Print the sorted array
-    ptr = arr;                          // Reset the pointer to the beginning of the array
-    printf("Sorted array: ");
-    while (ptr < (arr + size))          // Loop until the end of the array
-        printf("%d ", *ptr++);          // Print the current element and move the pointer
-    printf("\n");
-
-    // Free the dynamically allocated memory
-    free(arr);
+    free(str);
     return 0;
-}
-
-// Comparison function for qsort (ascending order)
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
 }
