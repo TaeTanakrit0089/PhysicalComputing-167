a2befcff1cc6367550cf3647e8d55d3ab8a3bd1d
Tae <65070089@kmitl.ac.th>
2024-09-03 14:40:40 +0700

    Updated Lab 09-01

--- labs/labs09-pointer/files/code.c
+++ labs/labs09-pointer/files/code.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
-    char *str;
+    int size, i;
+    int *arr;
 
-    // Allocate memory for the string "Porsche Arnold" (including null terminator)
-    str = (char*) malloc(15 * sizeof(char));
+    scanf("%d", &size);
 
-    // Copy "Porsche Arnold" into the allocated memory
-    strcpy(str, "Porsche Arnold");
+    // Allocate memory for the array
+    arr = (int*) malloc(size * sizeof(int));
 
-    // Print the string
-    char *ptr = str;
-    while (*ptr)
-        printf("%c", *ptr++);
-    // printf("%s", str);
+    // Populate the array
+    for (i = 0; i < size; i++)
+        *(arr + i) = i + 1;
 
-    // Free the allocated memory
-    free(str);
+    int *ptr = arr;  // Point to the first element of the array
+
+    // Print the array in order using pointers
+    printf("Array elements in order: ");
+    while (ptr < (arr+size))
+        printf("%d ", *ptr++);
 
     printf("\n");
+
+    // Print the array in reverse using pointers
+    printf("Array elements in reverse: ");
+    while (ptr > arr)
+        printf("%d ", *--ptr);  // Move the pointer to the previous element
+    printf("\n");
+
+    // Free the allocated memory
+    free(arr);
+
     return 0;
 }
