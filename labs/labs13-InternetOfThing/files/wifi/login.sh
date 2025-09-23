639902142a27603cccb6b50d2ffe07872ee549aa
tae <65070089@kmitl.ac.th>
2025-09-23 17:33:38 +0700

    Updated First Arduino Lab Details and Assignments

--- labs/labs13-InternetOfThing/files/wifi/login.sh
+++
@@ -1,3 +0,0 @@
-curl 'https://portal.it.kmitl.ac.th:4081/internal/dologin.php?NTLM=0&hash=individual_overall_5434' \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  --data-raw 'kerio_username=iot&kerio_password=iot'
